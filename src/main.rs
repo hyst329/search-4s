2af95f66119efe33671c89ad8b8a1f717280465c
hyst329 <trolleybus.1329@gmail.com>
2019-03-03 12:38:54 +0300

    basic form added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::request::LenientForm;
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+#[derive(FromForm)]
+struct SearchQuery {
+    query: String,
+}
+
+#[get("/")]
+fn index() -> Template {
+    let context = HashMap::<String, String>::new();
+    Template::render("index", context)
+}
+
+#[post("/search", data = "<query>")]
+fn search(query: LenientForm<SearchQuery>) -> String {
+    unimplemented!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+    .mount("/", routes![index])
+    .attach(Template::fairing())
+    .launch();
 }
